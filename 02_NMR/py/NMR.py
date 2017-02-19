6502fa5460d31eebbdd83f2e086688a5dcefe7c7
jacluff1 <jacluff1@asu.edu>
2017-02-19 04:20:23 -0700

    Close to being done

--- 02_NMR/py/NMR.py
+++ 02_NMR/py/NMR.py
@@ -2,7 +2,10 @@ import numpy as np
 import djak.plot as dp
 import djak.gen as dg
 import djak.fits as df 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import err as err
+
+# FITTING FUNCTIONS
 
 def quadfit(par,t):
 	return par[0]*(t-par[1])**2 + par[2]*(t-par[1]) + par[3]
@@ -10,238 +13,311 @@ def quadfit(par,t):
 def expfit(par,t):
 	return par[0]*np.exp(-t/par[1])
 
-def part4(plotA=True,printA=False,saveA=False,fs=20,size=(15,7.5)):
+def testfit(amount,index,par):
+	par[index] = par[index] + amount
+	return par
 
-	fig = plt.figure(figsize=size)
-	plt.title('$\\tau_1$ Two Pulses, Zero Crossing : Mineral Oil', fontsize=fs+2)
-	plt.xlabel('delay time [ms]', fontsize=fs)
-	plt.ylabel('amplitude [V]', fontsize=fs)
+# EXPERIMENTS
+
+def part4(plotA=True,printA=False,saveA=False,fs=20,size=(15,7.5)):
 	#plt.xscale('log')
 
 	tau = np.arange(7,26) # ms
+	tau = np.log(tau)
 	amp = np.array([3.8, 3.4, 2.9, 2.4, 2, 1.7, 1.2, .9, .5, .3, .4, .7, .9, 1.2, 1.5, 1.8, 2.0, 2.3, 2.6]) # V
-	plt.plot(tau,amp, 's', color='b', label='data')
+	amp = amp[2:]
+	tau = tau[2:]
 
 	T = np.linspace(5,30,1000)
-	par_m = [.05,16,0,.5]
+	T = np.log(T)
+	par_m = [12.5,2.8,0,.5]
 	A_m = quadfit(par_m,T)
-	plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
 
-	#print(len(tau),len(amp))
 	par_f = df.least_square(quadfit,par_m,tau,amp)
+	# testfit(2,0,par_f) # alpha
+	# testfit(.7,2,par_f) # beta
+	# testfit(.2,3,par_f) # A_0
+	# testfit(.02,1,par_f) # t_0
 	A_f = quadfit(par_f,T)
-	plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
 
-	alpha = par_f[0]
-	tau_1 = par_f[1]
-	A_0 = par_f[3]
-	note='$A = \\alpha (t-\\tau_1)^2 + A_0$\n$\\alpha = %s\ [V]$\n$\\tau_1 = %s\ [ms]$\n$A_0 = %s\ [V]$' % (alpha,tau_1,A_0)
-	plt.annotate(note, xy=(12,4), color='r', fontsize=fs)
+	alpha = dg.var('alpha',par_f[0],2,'V/s^2')
+	beta = dg.var('beta',par_f[2],.7,'V/s')
+	t_1 = dg.var('t_0',par_f[1],.02,'ms')
+	A_0 = dg.var('A_0',par_f[3],.2,'V')
 
-	plt.xlim([min(T),max(T)])
-	plt.legend(loc='best', numpoints=1)
-	plt.tight_layout()
+	if plotA == True:
+		fig = plt.figure(figsize=size)
+		plt.title('$\\tau_1$ Two Pulse, Zero Crossing : Mineral Oil', fontsize=fs+2)
+		plt.xlabel('ln(delay time) [ms]', fontsize=fs)
+		plt.ylabel('amplitude [V]', fontsize=fs)
+
+		plt.plot(tau,amp, 's', color='b', label='data')
+		plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
+		plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
+		note='$A = \\alpha (t_q-t_1)^2 + \\beta (t_q-t_1) + \gamma$\n$\\alpha = %s \pm %s\ [V/s^2]$\n$\\beta = %s \pm %s\ [V/s]$\n$\\gamma = %s \pm %s\ [V]$\n$t_1 = %s \pm %s\ [ms]$' % (alpha.pval,alpha.perr,beta.pval,beta.perr,A_0.pval,A_0.perr,t_1.pval,t_1.perr)
+		plt.annotate(note, xy=(2.5,4), color='r', fontsize=fs)
 	
-	if saveA == True:
-		fig.savefig('../graphs/fig01.png')
-		plt.close(fig)
-	else:
-		plt.show()
+		plt.xlim([min(T),max(T)])
+		plt.legend(loc='best', numpoints=1)
+		plt.tight_layout()
+	
+		if saveA == True:
+			fig.savefig('../graphs/fig01.png')
+			plt.close(fig)
+		else:
+			plt.show()
 
+	tau_1 = err.err4('part4',t_1)
+	tau1 = err.quad_err('part4',alpha,beta,A_0,tau,amp)
 
-	return
+	if printA == True:
+		print("tau_1 = %s +- %s" % (tau_1.pval,tau_1.perr))
+		print("tau1 = %s +- %s" % (tau1.pval,tau1.perr))
+
+	return tau_1,tau1
 
 def part5(plotA=True,printA=False,saveA=False,fs=20,size=(15,7.5)):
-
-	fig = plt.figure(figsize=size)
-	plt.title('$\\tau_2$ Two-Pulse Spin Echo : Mineral Oil', fontsize=fs+2)
-	plt.xlabel('delay time [ms]', fontsize=fs)
-	plt.ylabel('amplitude [V]', fontsize=fs)
-	#plt.xscale('log')
 
 	tau = np.arange(1,17) # ms
 	amp = np.array([7.5, 6.8, 6.0, 5.3, 4.9, 4.2, 4.0, 3.6, 3.4, 3.0, 2.8, 2.6, 2.4, 2.2, 2.0, 1.8]) # V
 
-	plt.plot(tau,amp, 's', color='b', label='data')
-
 	T = np.linspace(0,30,1000)
 	par_m = [8,10]
 	A_m = expfit(par_m,T)
-	plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
 
 	par_f = df.least_square(expfit,par_m,tau,amp)
+	# testfit(.2,0,par_f) # A_0
+	# testfit(.8,1,par_f) # tau_2
 	A_f = expfit(par_f,T)
-	plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
 
-	A_0 = par_f[0]
-	tau_2 = par_f[1]
-	note='$A = A_0 e^{-t/\\tau_2}$\n$A_0 = %s\ [V]$\n$\\tau_2 = %s\ [ms]$' % (A_0,tau_2)
-	plt.annotate(note, xy=(12,3), color='r', fontsize=fs)
+	A_0 = dg.var('A_0',par_f[0],.2,'V')
+	tau_2 = dg.var('part5',par_f[1],.8,'ms')
+	tau2 = err.exp_err(tau_2,A_0,tau,amp)
 
-	plt.xlim([min(T),max(T)])
-	plt.legend(loc='best', numpoints=1)
-	plt.tight_layout()
-	
-	if saveA == True:
-		fig.savefig('../graphs/fig02.png')
-		plt.close(fig)
-	else:
-		plt.show()
+	if plotA == True:
+		fig = plt.figure(figsize=size)
+		plt.title('$\\tau_2$ Two-Pulse Spin Echo : Mineral Oil', fontsize=fs+2)
+		plt.xlabel('delay time [ms]', fontsize=fs)
+		plt.ylabel('amplitude [V]', fontsize=fs)
+		
+		plt.plot(tau,amp, 's', color='b', label='data')
+		plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
+		plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
+		note='$A = A_0 e^{-t/\\tau_2}$\n$A_0 = %s \pm %s\ [V]$\n$\\tau_2 = %s \pm %s\ [ms]$' % (A_0.pval,A_0.perr,tau_2.pval,tau_2.perr)
+		plt.annotate(note, xy=(12,3), color='r', fontsize=fs)
+		
+		plt.xlim([min(T),max(T)])
+		plt.legend(loc='best', numpoints=1)
+		plt.tight_layout()
 
-	return
+		if saveA == True:
+			fig.savefig('../graphs/fig02.png')
+			plt.close(fig)
+		else:
+			plt.show()
+
+	if printA == True:
+		print("tau_2 = %s +- %s" % (tau_2.pval,tau_2.perr))
+		print("tau2 = %s +- %s" % (tau2.pval,tau2.perr))
+
+	return tau_2, tau2 
 
 def part6(plotA=True,printA=False,saveA=False,fs=20,size=(15,7.5)):
-
-	fig = plt.figure(figsize=size)
-	plt.title('$\\tau_2$ Carr-Purcell : Mineral Oil', fontsize=fs+2)
-	plt.xlabel('delay time [ms]', fontsize=fs)
-	plt.ylabel('amplitude [V]', fontsize=fs)
-	#plt.xscale('log')
 
 	tau = np.arange(20,120,20) # ms
 	amp = np.array([4.2, 2.10, 1.09, .62, .34]) # V
 
-	plt.plot(tau,amp, 's', color='b', label='data')
-
 	T = np.linspace(0,120,1000)
 	par_m = [8,30]
 	A_m = expfit(par_m,T)
-	plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
 
 	par_f = df.least_square(expfit,par_m,tau,amp)
+	# testfit(.5,0,par_f) # A_0
+	# testfit(2,1,par_f) # t_2
 	A_f = expfit(par_f,T)
-	plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
 
-	A_0 = par_f[0]
-	tau_2 = par_f[1]
-	note='$A = A_0 e^{-t/\\tau_2}$\n$A_0 = %s\ [V]$\n$\\tau_2 = %s\ [ms]$' % (A_0,tau_2)
-	plt.annotate(note, xy=(60,3), color='r', fontsize=fs)
+	A_0 = dg.var('A_0',par_f[0],.5,'V')
+	t_2 = dg.var('2 tau_2',par_f[1],2,'ms')
 
-	plt.xlim([min(T),max(T)])
-	plt.legend(loc='best', numpoints=1)
-	plt.tight_layout()
+	if plotA == True:
+		fig = plt.figure(figsize=size)
+		plt.title('$\\tau_2$ Carr-Purcell : Mineral Oil', fontsize=fs+2)
+		plt.xlabel('delay time [ms]', fontsize=fs)
+		plt.ylabel('amplitude [V]', fontsize=fs)
+
+		plt.plot(tau,amp, 's', color='b', label='data')
+		plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
+		plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
+		note='$A = A_0 e^{-t/2\\tau_2}$\n$A_0 = %s \pm %s\ [V]$\n$2\\tau_2 = %s \pm %s\ [ms]$' % (A_0.pval,A_0.perr,t_2.pval,t_2.perr)
+		plt.annotate(note, xy=(60,3), color='r', fontsize=fs)
+
+		plt.xlim([min(T),max(T)])
+		plt.legend(loc='best', numpoints=1)
+		plt.tight_layout()
 	
-	if saveA == True:
-		fig.savefig('../graphs/fig03.png')
-		plt.close(fig)
-	else:
-		plt.show()
+		if saveA == True:
+			fig.savefig('../graphs/fig03.png')
+			plt.close(fig)
+		else:
+			plt.show()
 
-	return
+	tau_2 = err.err6('part6',t_2)
+	tau2 = err.exp_err(tau_2,A_0,tau,amp,double='yes')
+
+	if printA == True:
+		print("tau2 = %s +- %s" % (tau_2.pval,tau_2.perr))
+		print("tau2 = %s +- %s" % (tau2.pval,tau2.perr))
+
+	return tau_2, tau2
 
 def part7(plotA=True,printA=False,saveA=False,fs=20,size=(15,7.5)):
-
-	fig = plt.figure(figsize=size)
-	plt.title('$\\tau_2$ Meiboom-Gill : Mineral Oil', fontsize=fs+2)
-	plt.xlabel('delay time [ms]', fontsize=fs)
-	plt.ylabel('amplitude [V]', fontsize=fs)
-	#plt.xscale('log')
 
 	tau = np.arange(20,124,20) # ms
 	amp = np.array([4.20, 2.39, 1.37, 1.00, .65, .35]) # V
 
-	plt.plot(tau,amp, 's', color='b', label='data')
-
 	T = np.linspace(0,120,1000)
 	par_m = [7,40]
 	A_m = expfit(par_m,T)
-	plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
 
 	par_f = df.least_square(expfit,par_m,tau,amp)
+	# testfit(.5,0,par_f) # A_0
+	# testfit(3,1,par_f) # t_2
 	A_f = expfit(par_f,T)
-	plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
 
-	A_0 = par_f[0]
-	tau_2 = par_f[1]
-	note='$A = A_0 e^{-t/\\tau_2}$\n$A_0 = %s\ [V]$\n$\\tau_2 = %s\ [ms]$' % (A_0,tau_2)
-	plt.annotate(note, xy=(60,2), color='r', fontsize=fs)
+	A_0 = dg.var('A_0',par_f[0],.5,'V')
+	t_2 = dg.var('2 tau_2',par_f[1],3,'ms')
 
-	plt.xlim([min(T),max(T)])
-	plt.legend(loc='best', numpoints=1)
-	plt.tight_layout()
+	if plotA == True:
+		fig = plt.figure(figsize=size)
+		plt.title('$\\tau_2$ Meiboom-Gill : Mineral Oil', fontsize=fs+2)
+		plt.xlabel('delay time [ms]', fontsize=fs)
+		plt.ylabel('amplitude [V]', fontsize=fs)
 	
-	if saveA == True:
-		fig.savefig('../graphs/fig04.png')
-		plt.close(fig)
-	else:
-		plt.show()
+		plt.plot(tau,amp, 's', color='b', label='data')
+		plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
+		plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
+		note='$A = A_0 e^{-t/2\\tau_2}$\n$A_0 = %s \pm %s\ [V]$\n$2\\tau_2 = %s \pm %s\ [ms]$' % (A_0.pval,A_0.perr,t_2.pval,t_2.perr)
+		plt.annotate(note, xy=(60,2), color='r', fontsize=fs)
 
-	return
+		plt.xlim([min(T),max(T)])
+		plt.legend(loc='best', numpoints=1)
+		plt.tight_layout()
+
+		if saveA == True:
+			fig.savefig('../graphs/fig04.png')
+			plt.close(fig)
+		else:
+			plt.show()
+
+	tau_2 = err.err7('part7',t_2)
+	tau2 = err.exp_err(tau_2,A_0,tau,amp,double='yes')
+
+	if printA == True:
+		print("tau2 = %s +- %s" % (tau_2.pval,tau_2.perr))
+		print("tau2 = %s +- %s" % (tau2.pval,tau2.perr))
+
+	return tau_2, tau2
 
 def part8a(plotA=True,printA=False,saveA=False,fs=20,size=(15,7.5)):
 
-	fig = plt.figure(figsize=size)
-	plt.title('$\\tau_1$ Two Pulses, Zero Crossing : Glycerin', fontsize=fs+2)
-	plt.xlabel('delay time [ms]', fontsize=fs)
-	plt.ylabel('amplitude [V]', fontsize=fs)
-	#plt.xscale('log')
-
 	tau = np.arange(12,25) # ms
+	tau = np.log(tau)
 	amp = np.array([2.98, 2.56, 2.14, 1.78, 1.42, 1.09, .62, .59, .82, 1.07, 1.29, 1.59, 1.90]) # V
-	plt.plot(tau,amp, 's', color='b', label='data')
 
 	T = np.linspace(10,25,1000)
-	par_m = [.05,19,0,.8]
+	T = np.log(T)
+	par_m = [10,np.log(19),0,.8]
 	A_m = quadfit(par_m,T)
-	plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
 
 	par_f = df.least_square(quadfit,par_m,tau,amp)
+	# testfit(1,0,par_f) # alpha
+	# testfit(.7,2,par_f) # beta
+	# testfit(.05,3,par_f) # A_0
+	# testfit(.01,1,par_f) # t_0
 	A_f = quadfit(par_f,T)
-	plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
 
-	alpha = par_f[0]
-	tau_1 = par_f[1]
-	A_0 = par_f[3]
-	note='$A = \\alpha (t-\\tau_1)^2 + A_0$\n$\\alpha = %s\ [V]$\n$\\tau_1 = %s\ [ms]$\n$A_0 = %s\ [V]$' % (alpha,tau_1,A_0)
-	plt.annotate(note, xy=(17,1.5), color='r', fontsize=fs)
+	alpha = dg.var('alpha',par_f[0],1,'V/s^2')
+	beta = dg.var('beta',par_f[2],.7,'V/s')
+	t_1 = dg.var('t_1',par_f[1],.01,'ms')
+	A_0 = dg.var('A_0',par_f[3],.05,'V')
 
-	plt.xlim([min(T),max(T)])
-	plt.legend(loc='best', numpoints=1)
-	plt.tight_layout()
-	
-	if saveA == True:
-		fig.savefig('../graphs/fig05.png')
-		plt.close(fig)
-	else:
-		plt.show()
+	if plotA == True:
+		fig = plt.figure(figsize=size)
+		plt.title('$\\tau_1$ Two Pulse, Zero Crossing : Glycerin', fontsize=fs+2)
+		plt.xlabel('ln(delay time) [ms]', fontsize=fs)
+		plt.ylabel('amplitude [V]', fontsize=fs)
 
-	return
+		plt.plot(tau,amp, 's', color='b', label='data')
+		plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
+		plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
+		note='$A = \\alpha (t_q-t_1)^2 + \\beta (t_q-t_1) + \gamma$\n$\\alpha = %s \pm %s\ [V/s^2]$\n$\\beta = %s \pm %s\ [V/s]$\n$\\gamma = %s \pm %s\ [V]$\n$t_1 = %s \pm %s\ [ms]$' % (alpha.pval,alpha.perr,beta.pval,beta.perr,A_0.pval,A_0.perr,t_1.pval,t_1.perr)
+		plt.annotate(note, xy=(2.8,1.5), color='r', fontsize=fs)
+
+		plt.xlim([min(T),max(T)]) 
+		plt.legend(loc='best', numpoints=1)
+		plt.tight_layout()
+
+		if saveA == True:
+			fig.savefig('../graphs/fig05.png')
+			plt.close(fig)
+		else:
+			plt.show()
+
+	tau_1 = err.err8a('part8a',t_1)
+	tau1 = err.quad_err('part8a',alpha,beta,A_0,tau,amp)
+
+	if printA == True:
+		print("tau_1_g = %s +- %s" % (tau_1.pval,tau_1.perr))
+		print("tau1_g = %s +- %s" % (tau1.pval,tau1.perr))
+
+	return tau_1, tau1
 
 def part8b(plotA=True,printA=False,saveA=False,fs=20,size=(15,7.5)):
-
-	fig = plt.figure(figsize=size)
-	plt.title('$\\tau_2$ Meiboom-Gill : Glycerin', fontsize=fs+2)
-	plt.xlabel('delay time [ms]', fontsize=fs)
-	plt.ylabel('amplitude [V]', fontsize=fs)
-	#plt.xscale('log')
 
 	tau = np.arange(20,100,20) # ms
 	amp = np.array([4, 1.71, .76, .43]) # V
 
-	plt.plot(tau,amp, 's', color='b', label='data')
-
 	T = np.linspace(0,120,1000)
 	par_m = [7,30]
 	A_m = expfit(par_m,T)
-	plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
 
 	par_f = df.least_square(expfit,par_m,tau,amp)
+	# testfit(.5,0,par_f) # A_0
+	# testfit(.8,1,par_f) # t_2
 	A_f = expfit(par_f,T)
-	plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
 
-	A_0 = par_f[0]
-	tau_2 = par_f[1]
-	note='$A = A_0 e^{-t/\\tau_2}$\n$A_0 = %s\ [V]$\n$\\tau_2 = %s\ [ms]$' % (A_0,tau_2)
-	plt.annotate(note, xy=(60,1.5), color='r', fontsize=fs)
+	A_0 = dg.var('A_0',par_f[0],.5,'V')
+	t_2 = dg.var('t_2',par_f[1],.8,'ms')
 
-	plt.xlim([min(T),max(T)])
-	plt.legend(loc='best', numpoints=1)
-	plt.tight_layout()
+	if plotA == True:
+		fig = plt.figure(figsize=size)
+		plt.title('$\\tau_2$ Meiboom-Gill : Glycerin', fontsize=fs+2)
+		plt.xlabel('delay time [ms]', fontsize=fs)
+		plt.ylabel('amplitude [V]', fontsize=fs)
+
+		plt.plot(tau,amp, 's', color='b', label='data')
+		plt.plot(T,A_m,'-', color='m', lw=2, label='manual fit')
+		plt.plot(T,A_f,'-', color='r', lw=2, label='least square fit')
+		note='$A = A_0 e^{-t/2\\tau_2}$\n$A_0 = %s \pm %s\ [V]$\n$2\\tau_2 = %s \pm %s\ [ms]$' % (A_0.pval,A_0.perr,t_2.pval,t_2.perr)
+		plt.annotate(note, xy=(60,1.5), color='r', fontsize=fs)
+
+		plt.xlim([min(T),max(T)])
+		plt.legend(loc='best', numpoints=1)
+		plt.tight_layout()
 	
-	if saveA == True:
-		fig.savefig('../graphs/fig06.png')
-		plt.close(fig)
-	else:
-		plt.show()
+		if saveA == True:
+			fig.savefig('../graphs/fig06.png')
+			plt.close(fig)
+		else:
+			plt.show()
 
-	return
+	tau_2 = err.err8b('part8b',t_2)
+	tau2 = err.exp_err(tau_2,A_0,tau,amp,double='yes')
+
+	if printA == True:
+		print("tau_2_g = %s +- %s" % (tau_2.pval,tau_2.perr))
+		print("tau2_g = %s +- %s" % (tau2.pval,tau2.perr))
+
+	return tau_2, tau2
+
+	
---
+++ 02_NMR/py/err.py
@@ -0,0 +1,98 @@
+import djak.gen as dg
+import numpy as np 
+
+# INSTRUMENT UNCERTAINTY
+
+dA = .005 # V
+dt = .5 # ms
+
+def err4(name,t):
+	"""calculates error for part4
+	args
+	----
+	name: name that goes into returned djak.gen.var object
+	t: djak.gen.var class object
+	"""
+	return dg.var(name,np.exp(t.val),np.exp(t.val)*t.err,'ms')
+
+def err6(name,t):
+	"""calculates error for part6
+	args
+	----
+	name: name that goes into returned djak.gen.var object
+	t: djak.gen.var class object
+	"""
+	return dg.var(name,(1/2)*t.val,(1/2)*t.err,'ms')
+
+err7 = err6
+ 
+err8a = err4
+
+err8b = err6
+
+def quad_err(name,alpha,beta,gamma,T_data,A_data):
+	T = np.array(T_data)
+	A = np.array(A_data)
+
+	short = np.sqrt(beta.val**2 - (4*alpha.val*(gamma.val-np.average(A))))
+
+	dt_q = dt/T
+
+	one = dt_q
+
+	two_1 = alpha.err/(2*alpha.val)
+	two_2 = (1/alpha.val) * (beta.val + short)
+	two_3 = (2*(gamma.val-A)) / short
+	two = two_1 * (two_2 + two_3)
+
+	three_1 = beta.err/(2*alpha.val)
+	three_2 = 1 + (beta.val*beta.err)/short
+	three = three_1 * three_2
+
+	four = (gamma.val*gamma.err)/short
+
+	five = (A*dA)/short
+
+	dT1 = np.sqrt(one**2 + two**2 + three**2 + four**2 + five**2)
+	tau1_err = np.exp(np.average(dT1)) * dt
+
+	T1 = T + (1/(2*alpha.val)) * (beta.val - short)
+	tau1_val = np.exp(np.average(T1))
+
+	tau1 = dg.var(name,tau1_val,tau1_err,'ms')
+
+	return tau1
+
+def exp_err(tau_2,A_0,T_data,A_data,double='no'):
+	T = np.array(T_data)
+	A = np.array(A_data)
+	# if double != 'no':
+	# 	T = T * (1/2)
+
+	# T2 = np.average(T) * np.log(np.average(A)/A_0.val)
+	# T2 = T * np.log(A/A_0.val)
+	T2 = -T/np.log(A/A_0.val)
+	print(T2)
+
+	one = dt * np.log(A/A_0.val)
+
+	two = T * dA/A
+
+	three = -T * A_0.err/A_0.val 
+
+	dT2 = np.sqrt(one**2 + two**2 + three**2)
+
+	tau2_val = np.average(T2)
+	tau2_err = np.average(dT2)
+	if double != 'no':
+		tau2_val *= 1/2
+		tau2_err *= 1/2
+
+	tau2 = dg.var(tau_2.name,tau2_val,tau2_err,'ms')
+
+	return tau2
+
+
+
+
+
